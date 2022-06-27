30477a1db22871df540f1299bda0fe1cf0c6f2fd
giomen88 <giorgia9288@virgilio.it>
2022-06-27 11:14:50 +0200

    correzioni in classe

--- js/dadi-script.js
+++ js/dadi-script.js
@@ -1,26 +1,19 @@
-const myRandom = Math.random();
-const pcRandom = Math.random();
-
 const min = 1;
 const max = 6;
 
-let message = 'The winner is '
-
-const myNumber = Math.floor(myRandom * (max + 1 - min)) + min;
+const myNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
 console.log(myNumber)
 
-const pcNumber = Math.floor(pcRandom * (max + 1 - min)) + min;
+const pcNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
 console.log(pcNumber)
+
+let message = 'The winner is '
 
 if (myNumber > pcNumber) {
     message += 'YOU'
-}
-
-else if (myNumber < pcNumber) {
+} else if (myNumber < pcNumber) {
     message += 'PC'
-}
-
-else {
+} else {
     message += 'NOBODY'
 }
 
--- js/email-script.js
+++ js/email-script.js
@@ -1,16 +1,37 @@
+// const emails = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "topolino@gmail.com"];
+
+// const userEmail = prompt('Inserisci la tua email');
+
+// let message;
+
+// if (emails.includes(userEmail)) {
+//     message = 'Benvenuto'
+// }
+
+// else {
+//     message = 'Spiacenti'
+// }
+
+// console.log(message)
+
+
 const emails = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "topolino@gmail.com"];
 
 const userEmail = prompt('Inserisci la tua email');
 
 let message;
 
-if (emails.includes(userEmail)) {
-    message = 'Benvenuto'
+let isAllowed = false
+for (let i = 0; i < emails.length && isAllowed === false; i++) {
+
+    if (userEmail == emails[i]) {
+        isAllowed = true
+        message = 'Benvenuto'
+    }
+
+    else {
+        message = 'Spiacenti'
+    }
 }
 
-else {
-    message = 'Spiacenti'
-}
-
-
-console.log(message)
+console.log(message) 
